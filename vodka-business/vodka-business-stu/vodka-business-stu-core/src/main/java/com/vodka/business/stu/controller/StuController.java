9cc20f6dd1dc4fb83196bfd61b0e350771c2e372
Breeze <BreezeWithDream@outlook.com>
2023-04-23 22:16:24 +0800

    配置文件整合Maven占位符

--- vodka-business/vodka-business-stu/vodka-business-stu-core/src/main/java/com/vodka/business/stu/controller/StuController.java
+++ vodka-business/vodka-business-stu/vodka-business-stu-core/src/main/java/com/vodka/business/stu/controller/StuController.java
@@ -1,5 +1,6 @@
 package com.vodka.business.stu.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/stu")
 public class StuController {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+
     @GetMapping("/list")
     public Object list() {
         System.out.println("list..");
+        System.out.println(dbUrl);
+
         return "stu list";
     }
 }
